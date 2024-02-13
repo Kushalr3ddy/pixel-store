c2dcb83a254ad5932d068f4e6ec210732aeeee8f
kushalr3ddy <pikkikushal@gmail.com>
2024-02-14 01:48:16 +0530

    4 pix works

--- test.py
+++ test.py
@@ -1,6 +1,26 @@
-from pixelstore import Encoder
 
 
-enc1 = Encoder(filename="bigc.pdf")
-enc1.encode()
 
+file = open("bigc.pdf","rb")
+raw_bytes =file.read(1)
+bit_sequence = []
+padded_bytes=[]
+count =0
+
+
+while raw_bytes:
+    curr_byte = bin(int.from_bytes(raw_bytes,byteorder="big"))
+    curr_byte = curr_byte[2:]
+    if len(curr_byte) < 8:
+        # fill up the lsb so if a byte is 1010 it will become 00001010
+        # this is done so it becomes 8 bits ie one whole byte for easy parsing later on
+        curr_byte = curr_byte.zfill(8)
+        padded_bytes.append(count)
+    #print((curr_byte))#,end=" ")
+    raw_bytes = file.read(1)
+    bit_sequence.append(curr_byte)
+
+
+for _ in bit_sequence:
+    print(_,end="")
+    exit(0)
