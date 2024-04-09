8a3db9721126d1aa343df6749885d54fe6df5f63
kushalr3ddy <pikkikushal@gmail.com>
2024-04-10 00:39:19 +0530

    checkpoint

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 from pixelstore.encoder import Encoder
+from pixelstore.decoder import Decoder
 
-
+import os
 #enc1 = Encoder(filename="bigc.pdf")
-enc1= Encoder(filename="payload.png")
-print(enc1.metadata)
-enc1.encode()
-
-#dec1=Decoder(filename="bigc.avi")
+#enc1= Encoder(filename="payload.png")
+#print(enc1.metadata)
+#enc1.encode()
+filename = os.path.join("output","bigc.avi")
+dec1=Decoder(filename=filename)
 #dec1.extract_frames()
-#print(dec1.metadata)
-
+print(dec1.metadata)
 
 
